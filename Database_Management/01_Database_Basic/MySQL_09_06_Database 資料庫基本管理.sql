6fd48255b738883f41df8e48e03f7e379ec3e220
u9828057 <u9828057@gmail.com>
2026-09-06 16:39:16 +0800

    Add files via upload

---
+++ Database_Management/01_Database_Basic/MySQL_09_06_Database 資料庫基本管理.sql
@@ -0,0 +1,16 @@
+-- ===========================
+-- MySQL Database 基本管理
+-- ===========================
+
+-- 1. 查看目前所有資料庫
+SHOW DATABASES;
+
+-- 2. 建立新的資料庫
+CREATE DATABASE mydb;
+
+-- 3. 使用資料庫
+USE mydb;
+
+-- 4. 刪除資料庫
+DROP DATABASE mydb;
+
